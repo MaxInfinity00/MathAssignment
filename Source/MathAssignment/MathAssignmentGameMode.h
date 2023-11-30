3b5d1f50fc2a4ac1379394a49995475c1b85ca0c
DESKTOP-M6O6AMR\Udit <uditshroff00@gmail.com>
2023-11-30 08:14:41 +0100

    assignment complete

---
+++ Source/MathAssignment/MathAssignmentGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MathAssignmentGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMathAssignmentGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMathAssignmentGameMode();
+};
+
+
+
